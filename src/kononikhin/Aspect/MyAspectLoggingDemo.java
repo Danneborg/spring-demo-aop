3f5d6099c5ccf8e7b9bea44e4be925482a69c035
Danila <whitetheia@gmail.com>
2021-02-02 21:56:44 +0700

    All aspects have been separated to different classes Added ordering to aspects

--- src/kononikhin/Aspect/MyAspectLoggingDemo.java
+++ src/kononikhin/Aspect/MyAspectLoggingDemo.java
@@ -1,8 +1,11 @@
 package kononikhin.Aspect;
 
+import kononikhin.Entity.Account;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -17,10 +20,26 @@ public class MyAspectLoggingDemo {
 //    }
 
     @Before("kononikhin.Aspect.AOPExressions.excludeGettersAndSetters()")
-    public void beforeExecution(){
+    public void beforeExecution(JoinPoint joinPoint) {
         System.out.println("=>>>>>> Before execution addAccount() <<<<<<=");
-    }
 
+        //display method signature
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("Metod " + methodSignature);
+
+        //display method arguments
+        Object[] args = joinPoint.getArgs();
+
+        for (Object arg : args) {
+            System.out.println(arg);
+            if(arg instanceof Account){
+                Account account = (Account)arg;
+                System.out.println("Account name " + account.getName());
+                System.out.println("Account level " + account.getLevel());
+            }
+        }
+
+    }
 
 
 }
