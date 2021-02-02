14e86888e2a76f1591186bd1bbf7ffdb5e012b0b
Danila <whitetheia@gmail.com>
2021-02-02 08:21:20 +0700

    All aspects have been separated to different classes Added ordering to aspects

--- src/kononikhin/Aspect/MyAspectLoggingDemo.java
+++ src/kononikhin/Aspect/MyAspectLoggingDemo.java
@@ -3,41 +3,24 @@ package kononikhin.Aspect;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(10)
 public class MyAspectLoggingDemo {
-
-    //any return type, any method name and any input parameters
-    @Pointcut("execution(* kononikhin.DAO.*.*(..))")
-    public void forDAOLayer(){};
-
-    //for getter methods
-    @Pointcut("execution(* kononikhin.DAO.*.get*(..))")
-    public void getters(){};
-
-    //for setter methods
-    @Pointcut("execution(* kononikhin.DAO.*.set*(..))")
-    public void setters(){};
-
-    //exclude getters and setters
-    @Pointcut("forDAOLayer() && !(setters() || getters())")
-    public void excludeGettersAndSetters(){};
 
 //    @Before("execution(* kononikhin.DAO.*.*(..))")
 //    public void beforeExecution(){
 //        System.out.println("=>>>>>> Before execution addAccount() <<<<<<=");
 //    }
 
-    @Before("excludeGettersAndSetters()")
+    @Before("kononikhin.Aspect.AOPExressions.excludeGettersAndSetters()")
     public void beforeExecution(){
         System.out.println("=>>>>>> Before execution addAccount() <<<<<<=");
     }
 
-    @Before("excludeGettersAndSetters()")
-    public void performeAPIanalytics(){
-        System.out.println("=>>>>>> Perform api analytitcs <<<<<<=");
 
-    }
+
 }
